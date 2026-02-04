edace2024f04b60b304cd04effd7e6c899a3b6d7
Robby Medich <robbymedich@gmail.com>
2026-02-04 07:40:56 -0500

    Update spacing sub-module

--- src/spacing.ts
+++ src/spacing.ts
@@ -1,7 +1,7 @@
 const leadingWhitespace = /\n{0,1}(?<indent>\s*)(?<line>.*)/g
 const isWhitespace = /^\s+$/
 
-function getIndentPrefix(text: string): string {
+export function getIndentPrefix(text: string): string {
     leadingWhitespace.lastIndex = 0
 
     for (const reMatch of text.matchAll(leadingWhitespace)) {
@@ -22,7 +22,7 @@ function getIndentPrefix(text: string): string {
     return ''
 }
 
-export function dedent(text: string, dedentPrefix?: string): string {
+export function dedentOld(text: string, dedentPrefix?: string): string {
     const indent =
         dedentPrefix === undefined ? getIndentPrefix(text) : dedentPrefix
     if (indent === '') {
@@ -46,4 +46,71 @@ export function indent(text: string, indentWith: string): string {
     return text.endsWith('\n')
         ? indentedText.slice(0, indentedText.length - indentWith.length)
         : indentedText
+}
+
+export function splitLines(text: string): string[] {
+    const length = '\n'.length
+    const result: string[] = []
+    let priorIx = 0
+
+    for (const match of text.matchAll(/\n/g)) {
+        const part = text.slice(priorIx, match.index + length)
+        if (part !== '') {
+            result.push(part)
+        }
+        priorIx = match.index + length
+    }
+    const part = text.slice(priorIx, text.length)
+    if (part !== '') {
+        result.push(part)
+    }
+    return result
+}
+
+export function dedent(text: string, dedentPrefix?: string): string {
+    const splitLine = /(?<indent>[^\S\n]*)(?<line>.*)/s
+    const indentLines = []
+    const textLines = []
+    let minIndent: string | null = null
+
+    for (const fullLineText of splitLines(text)) {
+        const reMatch = fullLineText.match(splitLine)
+        if (reMatch === null) {
+            throw new Error('all lines must match the splitLine pattern')
+        }
+        const indent = reMatch.groups!['indent']!
+        const line = reMatch.groups!['line']!
+
+        if (
+            fullLineText !== '\n' &&
+            (minIndent === null || indent.length < minIndent.length)
+        ) {
+            minIndent = indent
+        }
+        indentLines.push(indent)
+        textLines.push(line)
+        // console.log({fullLineText, indent, line})
+    }
+
+    let indentStart
+    if (minIndent == null) {
+        indentStart = 0
+    } else if (
+        typeof dedentPrefix !== 'string' ||
+        minIndent.length <= dedentPrefix.length
+    ) {
+        indentStart = minIndent.length
+    } else {
+        indentStart = dedentPrefix.length
+    }
+    const cleanedLines = []
+
+    for (let ix = 0; ix < textLines.length; ix++) {
+        const indent = indentLines[ix]!
+        const line = textLines[ix]!
+
+        cleanedLines.push(`${indent.slice(indentStart)}${line}`)
+    }
+
+    return cleanedLines.join('')
 }
---
+++ tests/spacing.test.ts
@@ -0,0 +1,91 @@
+import { expect, test, describe } from 'bun:test'
+import { splitLines, dedent } from '../src/spacing.ts'
+
+describe('splitLines', () => {
+    test('no new line', () => {
+        expect(splitLines('')).toEqual([])
+        expect(splitLines('hello')).toEqual(['hello'])
+    })
+
+    test('leading new line', () => {
+        expect(splitLines('\n    hello')).toEqual(['\n', '    hello'])
+        expect(splitLines('\n hello\nworld')).toEqual([
+            '\n',
+            ' hello\n',
+            'world',
+        ])
+    })
+
+    test('trailing new line', () => {
+        expect(splitLines('hello\n')).toEqual(['hello\n'])
+        expect(splitLines('\n  hello\n')).toEqual(['\n', '  hello\n'])
+        expect(splitLines('\n  hello\nworld\n')).toEqual([
+            '\n',
+            '  hello\n',
+            'world\n',
+        ])
+    })
+})
+
+describe('dedent', () => {
+    test('no new line', () => {
+        expect(dedent('')).toBe('')
+        expect(dedent('hello')).toBe('hello')
+        expect(dedent('    hello')).toBe('hello')
+    })
+
+    test('single new line', () => {
+        expect(dedent('\n')).toBe('\n')
+        expect(dedent('\nhello')).toBe('\nhello')
+        expect(dedent('\n    hello')).toBe('\nhello')
+    })
+
+    test('multiple new line', () => {
+        // only new lines
+        expect(dedent('\n\n')).toBe('\n\n')
+        // no spaces
+        expect(dedent('\nhello\nworld')).toBe('\nhello\nworld')
+        expect(dedent('hello\nworld')).toBe('hello\nworld')
+        expect(dedent('hello\nworld\n')).toBe('hello\nworld\n')
+        expect(dedent('\nhello\nworld\n')).toBe('\nhello\nworld\n')
+        // right spaces only
+        expect(dedent('\nhello\n    world')).toBe('\nhello\n    world')
+        expect(dedent('hello\n    world')).toBe('hello\n    world')
+        expect(dedent('hello\n    world\n')).toBe('hello\n    world\n')
+        expect(dedent('\nhello\n    world\n')).toBe('\nhello\n    world\n')
+        // left spaces only
+        expect(dedent('\n    hello\nworld')).toBe('\n    hello\nworld')
+        expect(dedent('    hello\nworld')).toBe('    hello\nworld')
+        expect(dedent('    hello\nworld\n')).toBe('    hello\nworld\n')
+        expect(dedent('\n    hello\nworld\n')).toBe('\n    hello\nworld\n')
+        // both spaces
+        expect(dedent('\n    hello\n    world')).toBe('\nhello\nworld')
+        expect(dedent('    hello\n    world')).toBe('hello\nworld')
+        expect(dedent('    hello\n    world\n')).toBe('hello\nworld\n')
+        expect(dedent('\n    hello\n    world\n')).toBe('\nhello\nworld\n')
+        // right spaces doubled
+        expect(dedent('\n    hello\n        world')).toBe('\nhello\n    world')
+        expect(dedent('    hello\n        world')).toBe('hello\n    world')
+        expect(dedent('    hello\n        world\n')).toBe('hello\n    world\n')
+        expect(dedent('\n    hello\n        world\n')).toBe(
+            '\nhello\n    world\n',
+        )
+        // left spaces doubled
+        expect(dedent('\n        hello\n    world')).toBe('\n    hello\nworld')
+        expect(dedent('        hello\n    world')).toBe('    hello\nworld')
+        expect(dedent('        hello\n    world\n')).toBe('    hello\nworld\n')
+        expect(dedent('\n        hello\n    world\n')).toBe(
+            '\n    hello\nworld\n',
+        )
+        // extra newlines
+        expect(dedent('\n    hello\n\n\n    world\n')).toBe(
+            '\nhello\n\n\nworld\n',
+        )
+        expect(dedent('    hello\n\n\n    world\n')).toBe(
+            'hello\n\n\nworld\n',
+        )
+        expect(dedent('    hello\n\n\n    world')).toBe(
+            'hello\n\n\nworld',
+        )
+    })
+})
