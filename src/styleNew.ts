d75033568dc0fe583d4c0b1db2fed1b520202af3
Robby Medich <robbymedich@gmail.com>
2026-02-24 06:52:42 -0500

    Make new api more friendly

--- src/styleNew.ts
+++ src/styleNew.ts
@@ -5,7 +5,9 @@ import { textModifiers } from './modifiers.js'
 
 export type LazyStyledText = {
     text: string
-    style?: Style
+    textColor?: Color
+    backgroundColor?: Color
+    fontStyles?: FontStyle[]
 }
 
 export type Style = {
@@ -14,6 +16,8 @@ export type Style = {
     fontStyles?: FontStyle[]
 }
 
+export type Stylist = ((text: string) => LazyStyledText) & (() => Style)
+
 // used to stay in sync with BackgroundColor type with Object.defineProperty
 function capitalize(value: string): string {
     return `${value.slice(0, 1).toUpperCase()}${value.slice(1)}`
@@ -21,29 +25,48 @@ function capitalize(value: string): string {
 
 type BackgroundColor = `bg${Capitalize<Color>}`
 
-type StyleBuilder = {
+export type StyleBuilder = {
     readonly [key in Color]: StyleBuilder
 } & {
     readonly [key in BackgroundColor]: StyleBuilder
 } & {
     readonly [key in FontStyle]: StyleBuilder
-} & (() => Style)
+} & (() => Stylist) &
+    ((text: string) => LazyStyledText)
 
-export const style = (function () {
-    let textColor: Color | undefined = undefined
-    let backgroundColor: Color | undefined = undefined
-    let fontStyles: FontStyle[] = []
+function createStylist(options?: Style) {
+    let textColor: Color | undefined = options?.textColor
+    let backgroundColor: Color | undefined = options?.backgroundColor
+    let fontStyles: FontStyle[] = options?.fontStyles ?? []
 
-    function build(): Style {
-        const out = {
-            textColor,
-            backgroundColor,
-            fontStyles: fontStyles.length === 0 ? undefined : fontStyles,
+    function build(optionalText?: string): LazyStyledText | Stylist {
+        if (optionalText !== undefined) {
+            return {
+                text: optionalText,
+                textColor,
+                backgroundColor,
+                fontStyles: fontStyles.length === 0 ? undefined : fontStyles,
+            }
         }
-        textColor = undefined
-        backgroundColor = undefined
-        fontStyles = []
-        return out
+        function stylist(): Style
+        function stylist(text: string): LazyStyledText
+        function stylist(text?: string): LazyStyledText | Style {
+            if (text === undefined) {
+                return {
+                    textColor,
+                    backgroundColor,
+                    fontStyles:
+                        fontStyles.length === 0 ? undefined : fontStyles,
+                }
+            }
+            return {
+                text,
+                textColor,
+                backgroundColor,
+                fontStyles: fontStyles.length === 0 ? undefined : fontStyles,
+            }
+        }
+        return stylist
     }
 
     // Set color options on the builder
@@ -85,6 +108,67 @@ export const style = (function () {
     }
 
     return build as StyleBuilder
+}
+
+export type StyleInitializer = {
+    readonly [key in Color]: StyleBuilder
+} & {
+    readonly [key in BackgroundColor]: StyleBuilder
+} & {
+    readonly [key in FontStyle]: StyleBuilder
+} & {
+    default: () => Stylist
+} & {
+    default: (text: string) => LazyStyledText
+}
+
+export const style = (function () {
+    const build = {}
+
+    // set default option
+    Object.defineProperty(build, 'default', {
+        get() {
+            return createStylist()
+        },
+        enumerable: true,
+    })
+
+    // Set color options on the builder
+    const defineColors = (color: Color) => {
+        Object.defineProperty(build, color, {
+            get() {
+                return createStylist({ textColor: color })
+            },
+            enumerable: true,
+        })
+        Object.defineProperty(build, `bg${capitalize(color)}`, {
+            get() {
+                return createStylist({ backgroundColor: color })
+            },
+            enumerable: true,
+        })
+    }
+    for (const color of Object.getOwnPropertyNames(colors)) {
+        defineColors(color as Color)
+    }
+
+    // Set font style options on the builder
+    const defineFontStyle = (modifier: FontStyle) => {
+        Object.defineProperty(build, modifier, {
+            get() {
+                return createStylist({ fontStyles: [modifier] })
+            },
+            enumerable: true,
+        })
+    }
+    for (const fontStyle of Object.getOwnPropertyNames(textModifiers)) {
+        if (fontStyle === 'reset') {
+            continue // reset is a text modifier but not a font style
+        }
+        defineFontStyle(fontStyle as FontStyle)
+    }
+
+    return build as StyleInitializer
 })()
 
 class StyledText {
@@ -98,17 +182,20 @@ class StyledText {
     // text(value: string)
 }
 
-function lazy(value: string, style?: Style): LazyStyledText {
-    return { text: value, style }
-}
-
 const theme = {
-    default: style(),
+    default: style.default(),
     important: style.bold.italic.red(),
 }
 const output = new StyledText(
-    lazy('hello world'),
-    lazy('this line is important', theme.important),
-    lazy('this line is not'),
+    theme.default('hello world'),
+    theme.important('this line is important'),
+    theme.default('this line is not'),
+    style.bgBlue('this has a blue background'),
 )
 console.log(output)
+
+console.log(
+    style.bgBlueBright('hi there' + JSON.stringify(style.red('hello there'))),
+)
+console.log(style.default('hi'))
+console.log(theme.important())
